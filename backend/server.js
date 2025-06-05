3800d331e1826e8f9f7f87b965f2eff20548a72e
unknown <tarunyad12345@gmail.com>
2025-06-06 01:38:34 +0530

    dummy route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const app = require('./src/app')
 
-
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus : true,
+        error : false
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
